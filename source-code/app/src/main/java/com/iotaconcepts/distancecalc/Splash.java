a33af124c273df14740efacd7c2da033fc3ef167
prasang7 <prasang.sharma7@gmail.com>
2017-01-23 10:52:05 +0530

    Stable

---
+++ source-code/app/src/main/java/com/iotaconcepts/distancecalc/Splash.java
@@ -0,0 +1,40 @@
+package com.iotaconcepts.distancecalc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Splash extends Activity{
+
+    Button getStarted;
+    TextView description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        });
+    }
+
+    private void init(){
+        setContentView(R.layout.splash_layout);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getStarted = (Button)findViewById(R.id.bt_splash_get_started);
+        description = (TextView)findViewById(R.id.tv_splash_descriptionOfApp);
+
+        Typeface Mont = Typeface.createFromAsset(getApplication().getAssets(), "Montserrat-Regular.otf");
+        getStarted.setTypeface(Mont);
+        description.setTypeface(Mont);
+    }
+}
